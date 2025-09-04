1641f5c855cc0cca0cbb4f3bd0cc1c8cabdf5995
Thiaga Manian <johndoe@example.com>
2025-09-04 10:23:13 -0400

    First LLM initial commit

---
+++ hello-world.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+load_dotenv(override=True)
+import os
+openai_api_key = os.getenv('OPENAI_API_KEY')
+
+if openai_api_key:
+    print(f"OpenAI API Key exists ")
+else:
+    print("OpenAI API Key not exists")
+
+
+from openai import OpenAI
+openai = OpenAI()
+messages = [{"role": "user", "content": "Print Hello World in any random non computer language. Mention the name of the language"}]
+
+
+response = openai.chat.completions.create(
+    model="gpt-5-nano",
+    messages=messages
+)
+
+print(response.choices[0].message.content)
